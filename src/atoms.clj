369acb2da4353491503992ad80f1d12c6563f1bc
José Javier Blanco Rivero <javierweiss@gmail.com>
2024-04-09 11:19:13 -0300

    notes

--- src/atoms.clj
+++ src/atoms.clj
@@ -2,7 +2,8 @@
   "Debugging this function fails throwing the following message:
       An error occurred in the breakpoint-finding logic. 
       We would love if you submitted an issue in the Calva 
-      repo with the instrumented code, or a similar reproducible case.")
+      repo with the instrumented code, or a similar reproducible case.
+   I tried this in WSL and in Gitpod Workspace-Full Docker container")
 
 (defn counter
   [top]
@@ -11,7 +12,7 @@
       (swap! cnt inc)
       (println @cnt))))
  
-(comment
+(comment 
    
   (counter 10)
 
